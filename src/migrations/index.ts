2598590d74458c33c0ca81ad79f4ca5d8cfbc947
Max Panfilov <m@panfilov.digital>
2025-11-05 11:44:31 +0300

    sort

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20251030_add_footer_contact_form from './20251030_add_footer_contact_form';
 import * as migration_20251101_113607 from './20251101_113607';
 import * as migration_20251102_150042 from './20251102_150042';
+import * as migration_20251105_084325 from './20251105_084325';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20251102_150042.up,
     down: migration_20251102_150042.down,
-    name: '20251102_150042'
+    name: '20251102_150042',
+  },
+  {
+    up: migration_20251105_084325.up,
+    down: migration_20251105_084325.down,
+    name: '20251105_084325'
   },
 ];
